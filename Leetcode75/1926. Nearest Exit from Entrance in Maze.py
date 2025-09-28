3830e5243b3c98ad5aa806b48b8be9c7c8502eba
Stellin <stellinjohn2003@gmail.com>
2025-09-28 13:23:33 +0400

    Refactor nearestExit method for improved readability and efficiency

--- Leetcode75/1926. Nearest Exit from Entrance in Maze.py
+++ Leetcode75/1926. Nearest Exit from Entrance in Maze.py
@@ -1,28 +1,27 @@
+from collections import deque
+
 class Solution(object):
     def nearestExit(self, maze, entrance):
-        """
-        :type maze: List[List[str]]
-        :type entrance: List[int]
-        :rtype: int
-        """
-        m, n = len(maze), len(maze[0])
+        m , n = len(maze) , len(maze[0])
         q = deque()
-        q.append((entrance[0], entrance[1], 0))
-        directions = [(-1,0), (1,0), (0,-1), (0,1)]
+        q.append((entrance[0],entrance[1],0))
+        visited = set()
+        visited.add((entrance[0],entrance[1]))
 
-        maze[entrance[0]][entrance[1]] = '+'
+        directions = [(1,0),(-1,0),(0,1),(0,-1)]
 
         while q:
-            r, c, steps = q.popleft()
+            r,c,steps = q.popleft()
 
             for dr, dc in directions:
                 nr, nc = r + dr, c + dc
 
-                if 0 <= nr < m and 0 <= nc < n and maze[nr][nc] == '.':
-                    if nr == 0 or nr == m - 1 or nc == 0 or nc == n - 1:
+                if 0 <= nr < m and 0 <= nc < n and maze[nr][nc] == "." and (nr,nc) not in visited:
+                    if nr == 0 or nr == m-1 or nc == 0 or nc == n-1 :
                         return steps + 1
-                    
-                    maze[nr][nc] = '+'
-                    q.append((nr, nc, steps + 1))
 
-        return -1
+                    visited.add((nr,nc))
+                    q.append((nr,nc,steps+1))
+
+        return -1  
+        
