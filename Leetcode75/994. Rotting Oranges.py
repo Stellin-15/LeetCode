a8f5163c774ff318389ad9bc9910e87a9047425b
Stellin <stellinjohn2003@gmail.com>
2025-09-28 14:44:21 +0400

    Refactor orangesRotting method for improved structure and clarity

--- Leetcode75/994. Rotting Oranges.py
+++ Leetcode75/994. Rotting Oranges.py
@@ -29,3 +29,34 @@ class Solution:
 
         # 3. Check if all fresh rotted
         return max_time if rotten == fresh else -1
+    
+
+from collections import deque
+class Solution(object):
+    def orangesRotting(self, grid):
+        m , n = len(grid) , len(grid[0])
+        q = deque()
+        fresh = 0
+
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == 2:
+                    q.append((i,j,0))
+                elif grid[i][j] == 1:
+                    fresh += 1
+        
+        directions = [(1,0),(-1,0),(0,1),(0,-1)]
+        time = 0
+
+        while q:
+            r,c,t = q.popleft()
+            time = t
+
+            for dr, dc in directions:
+                nr, nc = r + dr, c + dc
+                if 0 <= nr < m and 0 <= nc < n and grid[nr][nc] == 1:
+                    grid[nr][nc] = 2
+                    fresh -= 1
+                    q.append((nr,nc,t+1))
+        
+        return time if fresh == 0 else -1 
