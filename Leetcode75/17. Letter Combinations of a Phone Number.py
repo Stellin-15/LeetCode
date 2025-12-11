e315a515f47f44ba5aa55f0e797df6eb52f50155
Stellin <stellinjohn2003@gmail.com>
2025-12-11 20:55:46 +0400

    solved a mapping question

---
+++ Leetcode75/17. Letter Combinations of a Phone Number.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def letterCombinations(self, digits):
+
+        if not digits: 
+            return []
+        
+        mapping = {'2':"abc",'3':"def",'4':"ghi",'5':"jkl",'6':"mno",'7':"pqrs",'8':"tuv",'9':"wxyz"}
+        results = []
+
+        def backtrack(i, current):
+            if i == len(digits):
+                results.append(current)
+                return 
+        
+            for letters in mapping[digits[i]]:
+                backtrack(i+1, current + letters)
+
+        backtrack(0,"")
+        return results
+
+
+        
